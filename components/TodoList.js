e043193f448cb172c4749f038396683319c2d7df
thailh <thailh12@gmail.com>
2019-06-16 03:31:32 +0700

    fix todo list

--- components/TodoList.js
+++ components/TodoList.js
@@ -2,31 +2,29 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { CheckBox } from 'react-native-elements'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+
+const state = {
+  list: [
+    { description: 'Prepare an emergency kit', completed: false },
+    { description: 'Prepare a household flood plan', completed: false },
+    { description: 'Keep a list of emergency telephone numbers on display', completed: false },
+    {
+      description:
+        'Place important personal documents, valuables and vital medical supplies into a waterproof case in an accessible location',
+      completed: false,
+    },
+  ],
+}
 class TodoList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      list: [
-        { description: 'Prepare an emergency kit', completed: false },
-        { description: 'Prepare a household flood plan', completed: false },
-        { description: 'Keep a list of emergency telephone numbers on display', completed: false },
-        {
-          description:
-            'Place important personal documents, valuables and vital medical supplies into a waterproof case in an accessible location',
-          completed: false,
-        },
-      ],
-    }
-  }
   render() {
     return (
       <View>
-        {this.state.list.map((item, index) => {
+        {state.list.map((item, index) => {
           return (
             <TouchableOpacity
               key={index}
               onPress={() => {
-                this.state.list[index].completed = true
+                state.list[index].completed = true
                 this.forceUpdate()
               }}
             >
